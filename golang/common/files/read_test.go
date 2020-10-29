3a4785757ef52681b0afb3f034159c713bd9377f
Adriano Siqueira <dizaovp@gmail.com>
2020-10-29 16:58:50 -0300

    test: ensure Read return inputs on success

--- golang/common/files/read_test.go
+++ golang/common/files/read_test.go
@@ -20,15 +20,21 @@ func (m MockFileSystemObject) Open(name string) (file, error) {
 
 func TestRead(t *testing.T) {
 	t.Run("Should Read throws if Open throws", func(t *testing.T) {
-		mockPath := "input-000.txt"
+		mockPath := "input-000-test.txt"
 		mockFileSystem := &MockFileSystemObject{}
 		mockFileSystem.On("Open", mockPath).Return(os.Stdin, errors.New("Open throws")).Once()
-		fileRead := FileRead{
-			os: mockFileSystem,
-		}
+		fileRead := FileRead{os: mockFileSystem}
 		out, err := fileRead.Read(mockPath)
 		var a = assert.New(t)
 		a.Nil(out)
 		a.Error(err)
+	})
+
+	t.Run("Should Read return inputs on success", func(t *testing.T) {
+		fileRead := FileRead{os: osFS{}}
+		out, err := fileRead.Read("mock-input-000.txt")
+		var a = assert.New(t)
+		a.Nil(err)
+		a.NotNil(out)
 	})
 }
