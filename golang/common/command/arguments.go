bda6c71d050741e5ab9364ce457f15bcfc61ec54
Adriano Siqueira <dizaovp@gmail.com>
2020-10-30 12:25:30 -0300

    feat: ensure  Get throws if no parameters found

---
+++ golang/common/command/arguments.go
@@ -0,0 +1,33 @@
+package command
+
+import (
+	"errors"
+	"os"
+)
+
+var fs fileSystem = osFS{}
+
+type fileSystem interface {
+	Args() []string
+}
+
+type osFS struct{}
+
+func (osFS) Args() []string {
+	return os.Args
+}
+
+//Arguments is responsible for load services
+type Arguments struct {
+	os     fileSystem
+	params []string
+}
+
+//Get is responsible for get arguments line-command
+func (arguments *Arguments) Get() ([]string, error) {
+	arguments.params = arguments.os.Args()
+	if len(arguments.params) < 2 {
+		return nil, errors.New("no parameters found")
+	}
+	return arguments.params, nil
+}
