536b19896feca08bf23869559ce0d627d16d94ab
Adriano Siqueira <dizaovp@gmail.com>
2020-11-02 15:15:30 -0300

    test: ensure StringToJSON throws if SelectType throws

---
+++ golang/infra/convert/string_test.go
@@ -0,0 +1,23 @@
+package convert
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/adrianosiq/teste-backends/golang/infra"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestString(t *testing.T) {
+	t.Run("Should StringToJSON throws if SelectType throws", func(t *testing.T) {
+		mockString := &infra.MockString{}
+		mockString.On("Fields").Return(infra.MockFieldsExp).Once()
+		mockString.On("Split", infra.MockStringToJSON[0]).Return(infra.MockStringSplit).Once()
+		mockString.On("SelectType", "proposal", "created", infra.MockFieldsExp).Return([]string{}, errors.New("SelectType throws"))
+		str := String{deps: mockString}
+		out, err := str.StringToJSON(infra.MockStringToJSON)
+		var a = assert.New(t)
+		a.Error(err)
+		a.Equal([][]byte{}, out)
+	})
+}
