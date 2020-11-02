a6f1962c68a75bbe7dee2ae54df31a79c1107b2f
Adriano Siqueira <dizaovp@gmail.com>
2020-11-02 15:30:03 -0300

    test: ensure StringToJSON throws if CreateJSON throws

--- golang/infra/convert/string_test.go
+++ golang/infra/convert/string_test.go
@@ -20,4 +20,17 @@ func TestString(t *testing.T) {
 		a.Error(err)
 		a.Equal([][]byte{}, out)
 	})
+
+	t.Run("Should StringToJSON throws if CreateJSON throws", func(t *testing.T) {
+		mockString := &infra.MockString{}
+		mockString.On("Fields").Return(infra.MockFieldsExp).Once()
+		mockString.On("Split", infra.MockStringToJSON[0]).Return(infra.MockStringSplit).Once()
+		mockString.On("SelectType", "proposal", "created", infra.MockFieldsExp).Return(infra.MockSelectedFields, nil)
+		mockString.On("CreateJSON", infra.MockSelectedFields, infra.MockStringSplit).Return([]byte{}, errors.New("CreateJSON throws"))
+		str := String{deps: mockString}
+		out, err := str.StringToJSON(infra.MockStringToJSON)
+		var a = assert.New(t)
+		a.Error(err)
+		a.Equal([][]byte{}, out)
+	})
 }
