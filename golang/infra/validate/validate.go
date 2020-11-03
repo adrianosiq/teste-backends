6c1677788cf5cf6a046f37f604d44f4ac6f3ad59
Adriano Siqueira <dizaovp@gmail.com>
2020-11-03 17:11:27 -0300

    refactor: change validation events isoleted types

--- golang/infra/validate/validate.go
+++ golang/infra/validate/validate.go
@@ -5,55 +5,64 @@ import (
 	"github.com/google/uuid"
 )
 
-func removeEvent(checkEvent domain.Event, events []domain.Event) bool {
+func getEvents(events map[uuid.UUID]domain.Event) []domain.Event {
+	var result []domain.Event
 	for _, event := range events {
-		if event.EventSchema == "proposal" || event.EventAction != "removed" {
-			continue
-		}
-		if event.EventSchema == checkEvent.EventSchema && event.ProposalID == checkEvent.ProposalID && (checkEvent.WarrantyID == event.WarrantyID || event.ProponentID == event.ProponentID) {
+		result = append(result, event)
+	}
+	return result
+}
+
+func checkProposalDelete(events map[uuid.UUID]domain.Event) bool {
+	for _, event := range events {
+		if event.EventAction == "deleted" {
 			return true
 		}
 	}
 	return false
 }
 
-func updateEvent(updateEvent domain.Event, events []domain.Event) domain.Event {
+func getProposalEvent(events map[uuid.UUID]domain.Event) domain.Event {
+	var proposalID uuid.UUID
+	proposal := make(map[uuid.UUID]domain.Event)
 	for _, event := range events {
-		if event.EventAction != "updated" || event.EventSchema != updateEvent.EventSchema {
-			continue
-		}
-		if event.EventID == updateEvent.EventID && event.EventTimestamp.After(updateEvent.EventTimestamp) {
-			updateEvent = event
+		_, ok := proposal[event.ProposalID]
+		if !ok || (event.EventAction == "update" && event.EventTimestamp.After(proposal[event.ProposalID].EventTimestamp)) {
+			proposal[event.ProposalID] = event
+			proposalID = event.ProposalID
 		}
 	}
-	return updateEvent
+	return proposal[proposalID]
 }
 
-func filterEvents(events []domain.Event, schema string) []domain.Event {
-	filterEvents := make(map[uuid.UUID]domain.Event)
+func getProponentsEvents(events map[uuid.UUID]domain.Event) []domain.Event {
+	proponents := make(map[uuid.UUID]domain.Event)
 	for _, event := range events {
-		if event.EventSchema == schema {
-			_, ok := filterEvents[event.EventID]
-			if !ok && (event.EventAction == "created" || event.EventAction == "added") {
-				filterEvents[event.EventID] = event
-			}
+		_, ok := proponents[event.ProponentID]
+		if !ok || (event.EventAction == "update" && event.EventTimestamp.After(proponents[event.ProponentID].EventTimestamp)) {
+			proponents[event.ProponentID] = event
 		}
 	}
-	selected := []domain.Event{}
-	for _, event := range filterEvents {
-		if !removeEvent(event, events) || event.EventSchema == "proposal" {
-			event = updateEvent(event, events)
-			selected = append(selected, event)
+	return getEvents(proponents)
+}
+
+func getWarrantiesEvents(events map[uuid.UUID]domain.Event) []domain.Event {
+	warranties := make(map[uuid.UUID]domain.Event)
+	for _, event := range events {
+		_, ok := warranties[event.WarrantyID]
+		if !ok || (event.EventAction == "update" && event.EventTimestamp.After(warranties[event.WarrantyID].EventTimestamp)) {
+			warranties[event.WarrantyID] = event
 		}
 	}
-	return selected
+	return getEvents(warranties)
 }
 
 type validateInterface interface {
 	Group(events []domain.Event) map[uuid.UUID][]domain.Event
-	ProposalValidations(items []domain.Event) (domain.Event, bool)
-	ProponentValidations(items []domain.Event, proposal domain.Event) bool
-	WarrantyValidations(items []domain.Event, proposal domain.Event) bool
+	FilterEventType(events []domain.Event, schema string) map[uuid.UUID]domain.Event
+	ProposalValidations(events []domain.Event) (domain.Event, bool)
+	ProponentValidations(events []domain.Event, proposal domain.Event) bool
+	WarrantyValidations(events []domain.Event, proposal domain.Event) bool
 }
 
 type validateDependencies struct{}
@@ -66,11 +75,23 @@ func (d validateDependencies) Group(events []domain.Event) map[uuid.UUID][]domai
 	return group
 }
 
-func (d validateDependencies) ProposalValidations(items []domain.Event) (domain.Event, bool) {
-	if ok := ProposalDeletedValidation(items); !ok {
+func (d validateDependencies) FilterEventType(events []domain.Event, schema string) map[uuid.UUID]domain.Event {
+	filter := make(map[uuid.UUID]domain.Event)
+	for _, event := range events {
+		_, ok := filter[event.EventID]
+		if event.EventSchema == schema && !ok {
+			filter[event.EventID] = event
+		}
+	}
+	return filter
+}
+
+func (d validateDependencies) ProposalValidations(events []domain.Event) (domain.Event, bool) {
+	filterEvents := d.FilterEventType(events, "proposal")
+	if ok := checkProposalDelete(filterEvents); ok {
 		return domain.Event{}, false
 	}
-	proposal := filterEvents(items, "proposal")[0]
+	proposal := getProposalEvent(filterEvents)
 	if err := ProposalLoanValueValidation(proposal.ProposalLoanValue).Err; err != nil {
 		return domain.Event{}, false
 	}
@@ -80,28 +101,29 @@ func (d validateDependencies) ProposalValidations(items []domain.Event) (domain.
 	return proposal, true
 }
 
-func (d validateDependencies) ProponentValidations(items []domain.Event, proposal domain.Event) bool {
-	proponents := filterEvents(items, "proponent")
+func (d validateDependencies) ProponentValidations(events []domain.Event, proposal domain.Event) bool {
+	filterEvents := d.FilterEventType(events, "proponent")
+	proponents := getProponentsEvents(filterEvents)
 	if err := ProponentMinNumberValidation(len(proponents)).Err; err != nil {
 		return false
 	}
-	countProponentMain := 0
-	proponetMain := domain.Event{}
+	countMain := 0
+	var proponentMain domain.Event
 	for _, proponent := range proponents {
 		if err := ProponentAgeValidation(proponent.ProponentAge).Err; err != nil {
 			return false
 		}
 		if proponent.ProponentIsMain {
-			proponetMain = proponent
-			countProponentMain++
+			countMain++
+			proponentMain = proponent
 		}
 	}
-	if err := ProponentExactlyNumberMainValidation(countProponentMain).Err; err != nil {
+	if err := ProponentExactlyNumberMainValidation(countMain).Err; err != nil {
 		return false
 	}
 	mainIncomeAgeRange := MainIncomeAgeRange{
-		MainAge:             proponetMain.ProponentAge,
-		MonthlyIncome:       proponetMain.ProponentMonthlyIncome,
+		MainAge:             proponentMain.ProponentAge,
+		MonthlyIncome:       proponentMain.ProponentMonthlyIncome,
 		LoanValue:           proposal.ProposalLoanValue,
 		MonthlyInstallments: proposal.ProposalNumberOfMonthlyInstallments,
 	}
@@ -111,8 +133,9 @@ func (d validateDependencies) ProponentValidations(items []domain.Event, proposa
 	return true
 }
 
-func (d validateDependencies) WarrantyValidations(items []domain.Event, proposal domain.Event) bool {
-	warranties := filterEvents(items, "warranty")
+func (d validateDependencies) WarrantyValidations(events []domain.Event, proposal domain.Event) bool {
+	filterEvents := d.FilterEventType(events, "warranty")
+	warranties := getWarrantiesEvents(filterEvents)
 	if err := WarrantyMinValidation(len(warranties)).Err; err != nil {
 		return false
 	}
@@ -142,18 +165,18 @@ func NewValidate() Validate {
 }
 
 //Validation is responsible for return proposal approved
-func (validate Validate) Validation(events []domain.Event) []domain.ProposalApproved {
+func (validate Validate) Validation(data []domain.Event) []domain.ProposalApproved {
 	var approvals []domain.ProposalApproved
-	group := validate.deps.Group(events)
-	for proposalID, items := range group {
-		proposal, isProposalValid := validate.deps.ProposalValidations(items)
+	group := validate.deps.Group(data)
+	for proposalID, events := range group {
+		proposal, isProposalValid := validate.deps.ProposalValidations(events)
 		if !isProposalValid {
 			continue
 		}
-		if isProponentValid := validate.deps.ProponentValidations(items, proposal); !isProponentValid {
+		if isProponentValid := validate.deps.ProponentValidations(events, proposal); !isProponentValid {
 			continue
 		}
-		if warrantyIsValid := validate.deps.WarrantyValidations(items, proposal); !warrantyIsValid {
+		if isWarrantyValid := validate.deps.WarrantyValidations(events, proposal); !isWarrantyValid {
 			continue
 		}
 		approvals = append(approvals, domain.ProposalApproved{ProposalID: proposalID})
